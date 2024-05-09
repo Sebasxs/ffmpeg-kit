9793b2ce42020501a2d6fa525594a89b1d83d51c
Sebasxs <sebasxs@gmail.com>
2024-05-09 12:04:02 -0500

    chore: add Prettier configuration and update dependencies

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 3,
+  trailingComma: "all",
+  useTabs: false,
+};
+
+export default config;
