4f419173e0188f84c6b671eaf5c6db490810d6a1
Sebasxs <sebasxs@gmail.com>
2024-10-19 14:05:02 -0500

    fix: update atadenoise filter parameter to correct value

--- src/filters/denoise.ts
+++ src/filters/denoise.ts
@@ -2,7 +2,7 @@ import { DenoiseBuilder } from '@/types/filters';
 
 export const DenoiseFilter: DenoiseBuilder = (method) => {
    if (method === 'hqdn3d') return { videoFilter: 'hqdn3d=4:3:6:4.5', audioFilter: '' };
-   if (method === 'atadenoise') return { videoFilter: 'atadenoise=0.5', audioFilter: '' };
+   if (method === 'atadenoise') return { videoFilter: 'atadenoise=0.3', audioFilter: '' };
    if (method === 'nlmeans') return { videoFilter: 'nlmeans=s=7:p=9:pc=5', audioFilter: '' };
    return { audioFilter: 'afftdn=nt=w:tn=1', videoFilter: '' };
 };
